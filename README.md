02d083e37fd884d39f495bb165b57513cf39526f
Neil Han <neil_han@intuit.com>
2019-04-18 00:07:31 -0700

    init:cat:

---
+++ README.md
@@ -0,0 +1,6 @@
+# minimal cljs project with Lumo
+
+```
+npm install
+npx lumo -c src -m cljs_minimal.core
+```
---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject neilhan/cljs-minimal "0.0.1"
+  :description "A minimal ClojureScript project template"
+  :url "http://github.com/neilhan/cljs-minimal"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
+  :scm {:name "git"
+        :url "http://github.com/neilhan/cljs-minimal"}
+  :dependencies [[org.clojure/clojurescript "1.9.854"]])
